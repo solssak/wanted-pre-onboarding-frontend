fcd5ff384561810534375e03e69adc4f0fe44719
solssak <gswoong1012@gmail.com>
2023-04-09 17:47:48 +0900

    chore: Netlify 배포를 위한 useEffect 의존성 수정

--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -26,6 +26,7 @@ const SignUp = () => {
     if (isToken) {
       navigate("/todo", { replace: true });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const signUpHandler = (e) => {
