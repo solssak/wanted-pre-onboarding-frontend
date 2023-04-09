56adddb835caf20188a4a284061f085db93508e8
solssak <gswoong1012@gmail.com>
2023-04-09 17:56:21 +0900

    fix: Router 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import SignIn from "./pages/signin/SignIn";
 import SignUp from "./pages/signup/SignUp";
